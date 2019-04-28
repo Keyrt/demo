a5e2086c7a35a0fe908c69f8c026b311e9e39dbf
Keyrt <1208950226@qq.com>
2019-04-28 16:07:09 +0800

    demo

---
+++ demo3/demo.js
@@ -0,0 +1,26 @@
+var $wrap = $('.wrapper');
+
+setTimeout(function(){
+    $wrap.removeClass('init');
+},200);
+$('.item').on('click',function(){
+    $(this).addClass('active');
+    $wrap.addClass('wrapper-active');
+})
+
+$('.dir .close').on('click',function(e){
+    e.stopPropagation();
+    $('.active').removeClass('active');
+    $wrap.removeClass('wrapper-active');
+})
+
+
+
+
+
+
+
+
+
+
+
